a77a139a76211da38e67c3fb11f64824b22be83b
orduek <orduek@gmail.com>
2020-04-24 18:09:02 -0400

    Added basic scrubbing function

---
+++ scrubFunc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def scrub(file, thr=0.3):
+    # takes fmriPrep's FD and threshold (usually 0.3) and returns scrubbed column
+    regressor = pd.read_csv(file, sep=r'\s+')
+    x = np.zeros(len(regressor))
+    for i, fd in enumerate(regressor.framewise_displacement.fillna(0)):
+        
+        if fd>thr:
+            x[i] = 1
+            x[i-1] = 1
+            x[i+1] = 1
+        else:
+            continue
+    regressor['scrub'] = x
+    return regressor
+    regressor.to_csv(file, sep='\t', index=False)
